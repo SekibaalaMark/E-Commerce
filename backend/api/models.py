d46f9e213ae3877fa8d7cee2a49e0d5188f90d99
sekibaalamark <markmajje256@gmail.com>
2025-08-26 17:47:19 +0300

    created an abstract user model

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,3 +4,17 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+class CustomUser(AbstractUser):
+    ROLES = [('Admin','Admin'),('cashier','cashier'),('customer','customer')]
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    username = models.CharField(max_length=40,unique=True)
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=10,choices=ROLES)
+
+    def __str__(self):
+        return self.username
+    
+
+
+
