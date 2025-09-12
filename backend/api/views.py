ac738b613ab0e5e002f66405681ba53b7c1f4f59
sekibaalamark <markmajje256@gmail.com>
2025-09-12 17:27:23 +0300

    created a view for logging a user

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import CustomUserSerializer
+from .serializers import *
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class UserRegistrationView(APIView):
@@ -17,3 +18,28 @@ class UserRegistrationView(APIView):
             }
             return Response(response_data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+
+class LoginView(APIView):
+    def post(self,request):
+        serializer = loginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data["user"]
+
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            "message": "Login succesful",
+            "user":{
+                "id": user.id,
+                "username": user.username,
+                "role": user.role,
+                "email": user.email
+
+            },
+            "tokens":{
+                "refresh":str(refresh),
+                "access":str(refresh.token)
+            }
+        },status=status.HTTP_200_OK)
