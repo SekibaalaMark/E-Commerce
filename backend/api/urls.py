dc67342c48abb6f9a5cc172af93bd96e7fd9c8f4
sekibaalamark <markmajje256@gmail.com>
2025-09-12 17:27:51 +0300

    registered the login API in the urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register-user'),
+    path('login/',LoginView.as_view(),name="login-user")
 ]
